21d88344f84524804e315ef14353d58a33409b74
Vismayak Mohanarajan <vismayak97@gmail.com>
2022-04-08 13:51:46 -0500

    Hello world project

--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
+# Create your views here.
 
-def homePageView(request):
+
+def home_page_view(request):
     return HttpResponse("Hello, World!")
